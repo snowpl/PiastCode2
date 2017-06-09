225ade4d23c4cb91fa0eda905989006669f09a2f
snowpl <kacper.werema@gmail.com>
2017-06-10 00:51:19 +0200

    commit b4 changes

---
+++ Crud.Repository/OutgoingCommandRepository.cs
@@ -0,0 +1,25 @@
+﻿using Crud.Data.Models;
+using IData.Interfaces.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Repository
+{
+    public class OutgoingCommandRepository: IOutGoingCommandRepository
+    {
+        private readonly CrudDBContext _context;
+
+        public OutgoingCommandRepository(CrudDBContext context)
+        {
+            _context = context;
+        }
+
+        public void AddNewOutgoing()
+        {
+            
+        }
+    }
+}
