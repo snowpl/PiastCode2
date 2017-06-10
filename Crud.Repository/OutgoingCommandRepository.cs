6a7334128eebb40d66b3cbaa361333d50eea8c9b
snowpl <kacper.werema@gmail.com>
2017-06-10 02:10:16 +0200

    outgoing controller + repo

--- Crud.Repository/OutgoingCommandRepository.cs
+++ Crud.Repository/OutgoingCommandRepository.cs
@@ -1,37 +1,26 @@
-<<<<<<< HEAD
-﻿using System;
-=======
+
+using System;
 ﻿using Crud.Data.Models;
 using IData.Interfaces.Command;
 using System;
->>>>>>> dcf4e080205b36ed6b17562fc2664c46ada88e66
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using Crud.Data.Models;
 using IData.Interfaces.Command;
-
-namespace Crud.Repository
-{
-    public class OutgoingCommandRepository : IOutgoingCommandRepository
-    {
-        private CrudDBContext _context;
-=======
 
 namespace Crud.Repository
 {
     public class OutgoingCommandRepository: IOutGoingCommandRepository
     {
         private readonly CrudDBContext _context;
-
->>>>>>> dcf4e080205b36ed6b17562fc2664c46ada88e66
+        
         public OutgoingCommandRepository(CrudDBContext context)
         {
             _context = context;
         }
-<<<<<<< HEAD
+
         public void AddEditOutgoing(Outgoing outgoing)
         {
             if (outgoing.Id > 0)
@@ -71,12 +60,10 @@ namespace Crud.Repository
                 _context.Set<Outgoing>().Remove(current);
                 _context.SaveChanges();
             }
-=======
-
+        }
         public void AddNewOutgoing()
         {
             
->>>>>>> dcf4e080205b36ed6b17562fc2664c46ada88e66
         }
     }
 }
--- Crud/Controllers/OutgoingController.cs
+++ Crud/Controllers/OutgoingController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Crud.IServices;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,20 @@ namespace Crud.Controllers
 {
     public class OutgoingController : BaseController
     {
+        private readonly IOutgoingQueryServices _outgoingQueryServices;
+
+        public OutgoingController(
+            IOutgoingQueryServices outgoingQueryServices)
+        {
+            _outgoingQueryServices = outgoingQueryServices;
+        }
+
         [HttpGet]
         public JsonResult GetOutgoings()
         {
-            
+            var result = _outgoingQueryServices.GetAllOutgoings();
+            var jsonresult = JsonConvert.SerializeObject(result);
+            return Json(jsonresult, JsonRequestBehavior.AllowGet);
         }
     }
 }
